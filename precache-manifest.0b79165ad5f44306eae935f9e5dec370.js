37d26bafa54a7a382391fc670610fbb7aa391441
Semen Merkulov <semen.merkulov@sequenia.com>
2020-10-03 14:49:10 +0700

    Updates

---
+++ precache-manifest.0b79165ad5f44306eae935f9e5dec370.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296ba99c109989f3705be60bdee51e74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "52e98b9c43021d0ae158",
+    "url": "./static/css/2.dcab9049.chunk.css"
+  },
+  {
+    "revision": "b917c91b71bda1681d8b",
+    "url": "./static/css/main.e8dc666a.chunk.css"
+  },
+  {
+    "revision": "52e98b9c43021d0ae158",
+    "url": "./static/js/2.2f081c3b.chunk.js"
+  },
+  {
+    "revision": "082c1bdf24861af2babbd4591bc978fa",
+    "url": "./static/js/2.2f081c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b917c91b71bda1681d8b",
+    "url": "./static/js/main.a4e5f072.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "./static/js/main.a4e5f072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6374355ce062724bfe7a",
+    "url": "./static/js/runtime-main.c3e24b0c.js"
+  }
+]);
